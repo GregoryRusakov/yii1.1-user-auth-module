e247caa1b2b843f1cca1e230a69ec6bdd48d408a
greg <greg@server-5>
2015-06-16 23:55:52 +0500

    150617 0055 server

--- models/Invitations.php
+++ models/Invitations.php
@@ -65,7 +65,7 @@ class Invitations extends CActiveRecord
 		return array(
 			'guid' => Yii::t('AuthModule.forms', 'Invitation'),
 			'date_issued' => Yii::t('AuthModule.forms', 'Date issued'),
-                        'Used'=>Yii::t('AuthModule.forms', 'Used'),
+                        'used'=>Yii::t('AuthModule.forms', 'Used'),
                         'verifyCode'=>Yii::t('AuthModule.forms', 'Verify code'),
                         'date_occuped' => Yii::t('AuthModule.forms', 'Date occuped'),
 			'username_created' => Yii::t('AuthModule.forms', 'Created username'),
